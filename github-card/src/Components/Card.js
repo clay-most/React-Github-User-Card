f9195edc72097b6de74250dde9e36b9a67069e64
Anna Morris <annapatton444@gmail.com>
2019-12-11 18:12:41 -0600

    Update Card.js

--- github-card/src/Components/Card.js
+++ github-card/src/Components/Card.js
@@ -2,7 +2,7 @@ import React from "react";
 
 class Card extends React.Component {
   render() {
-    return (<div className="Card">{this.props.user.login}</div>);
+    return <div className="Card">{this.props.user.login}</div>;
   }
 }
 
